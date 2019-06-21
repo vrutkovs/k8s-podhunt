f1c0f762331222d998d4fdaa63cb87272c07ccea
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-06-21 13:30:15 +0200

    Implement killRandomPod

--- k8s.go
+++ k8s.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
+	"time"
+
 	k8s "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -12,15 +17,32 @@ const (
 
 
 func inClusterLogin() (*k8s.Clientset, error) {
-  // creates the in-cluster config
-  config, err := rest.InClusterConfig()
-  if err != nil {
-    panic(err.Error())
-  }
-  // creates the clientset
-  return k8s.NewForConfig(config)
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+	// creates the clientset
+	return k8s.NewForConfig(config)
 }
 
-func killRandomPod(c *k8s.Clientset) error {
-  return nil
+func killRandomPod(c *k8s.Clientset) (string, error) {
+	// Seed random
+	rand.Seed(time.Now().Unix())
+
+	// Find random pod
+	pods, err := c.CoreV1().Pods("").List(metav1.ListOptions{})
+	if err != nil {
+		return "", err
+	}
+	if pods.Items == nil || len(pods.Items) == 0 {
+		return "", fmt.Errorf("No pods fetched")
+	}
+	randomPod := pods.Items[rand.Intn(len(pods.Items))]
+
+	// Kill kill kill
+	if err := c.CoreV1().Pods(randomPod.Namespace).Delete(randomPod.Name, &metav1.DeleteOptions{}); err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("Killed %s in %s namespace", randomPod.Name, randomPod.Namespace), nil
 }
--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"math/rand"
 	"fmt"
+	"math/rand"
 	"io"
 	"log"
 	"net/http"
@@ -10,33 +10,39 @@ import (
 )
 
 func handleKill(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("headers: %v\n", r.Header)
-
 	_, err := io.Copy(os.Stdout, r.Body)
 	if err != nil {
+		log.Println("Failed to parse the request")
 		log.Println(err)
 		return
 	}
 
-  c, err := inClusterLogin()
-  if err != nil {
-    panic(err.Error())
-  }
+	c, err := inClusterLogin()
+	if err != nil {
+		log.Println("Failed to login in cluster")
+		log.Println(err)
+		return
+	}
 
-  switch rand.Intn(killOptions) {
-  case 0:
-    killRandomPod(c)
-  case 1:
-    //killRandomDeployment(c)
-  case 2:
-    //killRandomStatefulSet(c)
-  case 3:
-    //updateRandomDeployement(c)
-  case 4:
-    //updateRandomStatefulSet(c)
-  case 5:
-    //killRandomWorker(c)
-  }
+	message := ""
+
+	switch rand.Intn(killOptions) {
+	case 0:
+		message, err = killRandomPod(c)
+	case 1:
+		//killRandomDeployment(c)
+	case 2:
+		//killRandomStatefulSet(c)
+	case 3:
+		//updateRandomDeployement(c)
+	case 4:
+		//updateRandomStatefulSet(c)
+	case 5:
+		//killRandomWorker(c)
+	}
+	if err != nil {
+		fmt.Fprintf(w, "{'message': '%s'}", message)
+	}
 }
 
 func main() {
