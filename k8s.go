a25f1800868859ad80ebff52dab28e91dce64f70
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-06-22 22:32:38 +0200

    Reorder imports

--- k8s.go
+++ k8s.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
-	"time"
 	"sort"
+	"time"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8s "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var blackListedNamespaces = []string{"openshift-etcd"}
